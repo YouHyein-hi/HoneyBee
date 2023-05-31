4dcf537377fe2d141022e033075a1d152f4c6be0
hyein You <laksjd4560@gmail.com>
2023-05-31 17:03:08 +0900

    [feat]Room 변경, changeServerData 수정

--- data/src/main/java/com/example/data/module/LocalDataModule.kt
+++ data/src/main/java/com/example/data/module/LocalDataModule.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import androidx.room.Room
 import com.example.data.local.BitmapConverter
 import com.example.data.local.LocalDatabase
+import com.example.data.local.migration_2_3
 import com.example.data.module.RetrofitModule.gson
 //import com.example.data.local.LocalDateTimeConverter
 //import com.example.data.local.MultipartBodyConverter
@@ -34,6 +35,7 @@ object LocalDataModule {
     @Provides
     fun provideLocalDatabase(@ApplicationContext appContext: Context): LocalDatabase{
         return Room.databaseBuilder(appContext, LocalDatabase::class.java, "mymymyDB")
+            .addMigrations(migration_2_3)
             // .fallbackToDestructiveMigration()
             // => 이걸쓰면 테이블이 유실되어 호출 실패해도 db를 재생성함(이전데이터 날라감)
             //.addTypeConverter(LocalDateTimeConverter(gson))

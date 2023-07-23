4aae0fab5b8edeec1ab570be232a29fd0fb6485f
hyein You <laksjd4560@gmail.com>
2023-07-23 23:43:31 +0900

    [feat] 권한 Dialog 추가

---
+++ app/src/main/java/com/example/receiptcareapp/ui/dialog/Permissiond_Dialog.kt
@@ -0,0 +1,54 @@
+package com.example.receiptcareapp.ui.dialog
+
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.ViewGroup
+import com.example.receiptcareapp.base.BaseDialog
+import com.example.receiptcareapp.databinding.DialogCardBinding
+import com.example.receiptcareapp.databinding.DialogPermissionBinding
+
+class Permissiond_Dialog : BaseDialog<DialogPermissionBinding>(DialogPermissionBinding::inflate)  {
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val CAMERA = arrayOf(android.Manifest.permission.CAMERA)
+    private val GALLERY = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val CAMERA_CODE = 98
+    private val GALLERY_CODE = 1010
+
+    // 인터페이스를 정의하여 이벤트를 처리하기 위한 콜백 메서드를 포함합니다.
+    interface OnDismissListener {
+        fun onDialogDismissed()
+    }
+    // 이 콜백을 호출할 변수를 선언합니다.
+    private var onDismissListener: OnDismissListener? = null
+    // 이벤트를 처리하기 위한 콜백을 등록하는 메서드를 정의합니다.
+    fun setOnDismissListener(listener: OnDismissListener) {
+        onDismissListener = listener
+    }
+
+    override fun initData() {
+    }
+
+    override fun initUI() {
+        val width = resources.displayMetrics.widthPixels
+        dialog?.window?.setLayout((width * 1), ViewGroup.LayoutParams.WRAP_CONTENT)
+
+    }
+
+    override fun initListener() {
+        binding.permissionButton.setOnClickListener {
+            // 다이얼로그가 닫힐 때 콜백 호출
+            onDismissListener?.onDialogDismissed()
+            dismiss()
+        }
+    }
+
+    override fun initObserver() {
+
+    }
+
+}

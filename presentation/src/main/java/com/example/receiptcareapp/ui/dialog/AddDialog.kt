5cf91fa96878b119b6218d25e62b7b16588b77d4
hyein You <laksjd4560@gmail.com>
2023-08-08 14:43:05 +0900

    [style, feat] 메인(홈) 페이지 수정 (UI, 기능 등)

---
+++ presentation/src/main/java/com/example/receiptcareapp/ui/dialog/AddDialog.kt
@@ -0,0 +1,30 @@
+package com.example.receiptcareapp.ui.dialog
+
+import androidx.navigation.fragment.findNavController
+import com.example.receiptcareapp.R
+import com.example.receiptcareapp.base.BaseDialog
+import com.example.receiptcareapp.databinding.DialogAddBinding
+
+class AddDialog : BaseDialog<DialogAddBinding>(DialogAddBinding::inflate) {
+
+    override fun initData() {
+
+    }
+
+    override fun initUI() {
+
+    }
+
+    override fun initListener() {
+        with(binding){
+            homeAddCamera.setOnClickListener{  findNavController().navigate(R.id.action_homeFragment_to_cameraFragment)}
+            homeAddGallery.setOnClickListener{  findNavController().navigate(R.id.action_homeFragment_to_galleryFragment)}
+        }
+
+    }
+
+    override fun initObserver() {
+
+    }
+
+}

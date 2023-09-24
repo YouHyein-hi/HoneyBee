f66326ae509746fbe8282fcc84df405fdf4fe829
hyein You <laksjd4560@gmail.com>
2023-09-25 00:38:45 +0900

    [feat] 카드 추가에 카드 이미지도 추가 (나중에 이미지들 수정)

---
+++ domain/src/main/java/com/example/domain/model/ui/recycler/CardImageRecyclerData.kt
@@ -0,0 +1,5 @@
+package com.example.domain.model.ui.recycler
+
+data class CardImageRecyclerData(
+    val cardImageId : Int
+)
--- presentation/src/main/java/com/example/receiptcareapp/ui/dialog/CardAddDialog.kt
+++ presentation/src/main/java/com/example/receiptcareapp/ui/dialog/CardAddDialog.kt
@@ -2,6 +2,7 @@ package com.example.receiptcareapp.ui.dialog
 
 import android.util.Log
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.domain.model.remote.send.card.SendCardData
 import com.example.domain.util.StringUtil
 import com.example.receiptcareapp.R
@@ -12,6 +13,9 @@ import com.example.receiptcareapp.state.ResponseState
 import com.example.receiptcareapp.viewModel.dialogViewModel.CardAddViewModel
 import com.example.receiptcareapp.viewModel.fragmentViewModel.CardViewModel
 import com.example.domain.model.ui.dateTime.DateData
+import com.example.domain.model.ui.recycler.CardImageRecyclerData
+import com.example.receiptcareapp.ui.adapter.CardImageAdapter
+import com.example.receiptcareapp.ui.adapter.RecordLocalAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,8 +23,18 @@ class CardAddDialog(
     private val cardViewModel: CardViewModel
 ) : BaseDialog<DialogCardAddBinding>(DialogCardAddBinding::inflate) {
 
+    // TODO 나중에 imageResIds 부분 하드코딩이라 이걸로 수정하자...
+/*    private val imageList: List<CardImageRecyclerData> by lazy {
+        val imageIds = resources.getIntArray(R.array.card_image_array_id)
+        imageIds.map { CardImageRecyclerData(it) }
+    }
+
+    private val adapter: CardImageAdapter by lazy {
+        CardImageAdapter(imageList)
+    }*/
     private val viewModel : CardAddViewModel by viewModels()
     private lateinit var dateData : DateData
+    private var cardImage = 1
 
     override fun initData() {
     }
@@ -28,10 +42,25 @@ class CardAddDialog(
     override fun initUI() {
         val date = StringUtil.dateNow()
         binding.cardAddDateDatePicker.init(date.year, date.monthValue-1, date.dayOfMonth, null)
+        binding.image = cardImage
     }
 
     override fun initListener() {
+
+        val imageResIds = listOf(
+            R.drawable.card_image_1,
+            R.drawable.card_image_2,
+            R.drawable.card_image_3
+        )
+
+        binding.cardAddImageRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        val cardImageAdapter = CardImageAdapter(imageResIds)
+        binding.cardAddImageRecyclerView.adapter = cardImageAdapter
+
         with(binding){
+
+            cardAddImageTxt.text = "${cardImage}번 그림을 선택하셨습니다!"
+
             cardAddOkBtn.setOnClickListener{
                 var price = cardAddPriceEdit.text.toString()
                 var priceZero = price.count { it == '0' }
@@ -54,7 +83,7 @@ class CardAddDialog(
                 else{
                     if (price.contains(","))
                         price = price.replace(",", "")
-                    cardViewModel.insertServerCardData(SendCardData(cardAddNameEdit.text.toString(), price.toInt(), myDateTime!!, 1))
+                    cardViewModel.insertServerCardData(SendCardData(cardAddNameEdit.text.toString(), price.toInt(), myDateTime!!, cardImage))
                     dismiss()
                 }
             }
@@ -63,6 +92,13 @@ class CardAddDialog(
                 Log.e("TAG", "onResume: 카드 추가 취소", )
                 dismiss()
             }
+
+            cardImageAdapter.onCardImageClick = {
+                Log.e("TAG", "initListener: ${it}", )
+                cardImage = it
+                image = it
+            }
+
         }
     }
 
